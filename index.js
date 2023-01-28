11e3515842ab2ea6d01d6c8b457309600c37a712
RAJESH KUMAR SAHOO <rajeshkumarsahoo006@gmail.com>
2023-01-28 15:14:20 +0530

    BackEnd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //=====================Importing Module and Packages=====================//
 const express = require('express');
 const route = require('./src/routes/route.js');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 require("dotenv").config();
 const cors = require('cors')
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json());
 
 mongoose.set('strictQuery', false)
-mongoose.connect("mongodb+srv://raj_3028:kWaM507ps0Icsdg0@cluster0.pw23ckf.mongodb.net/UW-Infotech-DB", { useNewUrlParser: true })
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
     .then(() => console.log("MongoDb is Connected."))
     .catch(error => console.log(error))
 
